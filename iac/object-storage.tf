e93ea8a517f372320021285a498ad851596572aa
Felipe Vilarinho <fvilarin@akamai.com>
2024-10-21 01:59:54 -0300

    Adding firewall provisioning.

--- iac/object-storage.tf
+++ iac/object-storage.tf
@@ -1,3 +1,7 @@
+locals {
+  backupCredentialsFilename = abspath(pathexpand("../etc/.backupCredentials"))
+}
+
 # Definition of the object storage bucket for backup.
 resource "linode_object_storage_bucket" "backup" {
   label  = "${var.settings.cluster.namespace}-${var.settings.cluster.identifier}-backup"
@@ -15,4 +19,15 @@ resource "linode_object_storage_key" "backup" {
   }
 
   depends_on = [ linode_object_storage_bucket.backup ]
+}
+
+# Creates the backup credentials file.
+resource "local_sensitive_file" "backupCredentials" {
+  filename = local.backupCredentialsFilename
+  content = <<EOT
+[default]
+aws_access_key_id=${linode_object_storage_key.backup.access_key}
+aws_secret_access_key=${linode_object_storage_key.backup.secret_key}
+EOT
+  depends_on = [ linode_object_storage_key.backup ]
 }
