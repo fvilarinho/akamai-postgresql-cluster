f7b0458475a162d974dbed64ff5d8f0129023ccd
Felipe Vilarinho <fvilarin@akamai.com>
2024-10-25 17:17:11 -0300

    Code improvements.

---
+++ bin/applyStackServices.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Check the dependencies of this script.
+function checkDependencies() {
+  if [ -z "$KUBECONFIG" ]; then
+    echo "The cluster configuration file is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$NAMESPACE" ]; then
+    echo "The stack namespace is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$IDENTIFIER" ]; then
+    echo "The stack identifier is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$MANIFEST_FILENAME" ]; then
+    echo "The manifest file is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$DATABASE_PORT" ]; then
+    echo "The database port is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+}
+
+# Applies the stack services replacing the placeholders with the correspondent environment variable value.
+function applyStackServices() {
+  manifestFilename="$MANIFEST_FILENAME"
+
+  cp -f "$manifestFilename" "$manifestFilename".tmp
+
+  sed -i -e 's|${NAMESPACE}|'"$NAMESPACE"'|g' "$manifestFilename".tmp
+  sed -i -e 's|${IDENTIFIER}|'"$IDENTIFIER"'|g' "$manifestFilename".tmp
+  sed -i -e 's|${DATABASE_PORT}|'"$DATABASE_PORT"'|g' "$manifestFilename".tmp
+
+  $KUBECTL_CMD apply -f "$manifestFilename".tmp
+
+  rm -f "$manifestFilename".tmp*
+}
+
+# Main function.
+function main() {
+  checkDependencies
+  applyStackServices
+}
+
+main
